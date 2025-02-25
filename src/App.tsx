9eb044720a91813051663cdc017527a9370ae4f6
im <omarainnocent644@gmail.com>
2025-02-25 16:52:20 +0300

    nothing in the working tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { IoIosAddCircle } from "react-icons/io";
-import { FaDeleteLeft} from "react-icons/fa6";
-import {  useEffect, useState } from 'react';
+import { FaDeleteLeft } from "react-icons/fa6";
+import { useEffect, useState } from 'react';
 interface Todo {
   _id: number;
   title: string;
@@ -9,40 +9,50 @@ interface Todo {
 export default function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [newTodo, setNewTodo] = useState<string>('');
-  const addTodo = () => {
-  };
-  const deleteTodo = (_id: number) => {
-   
+
+//get req//
+  const fetchAllData = async () => {
+    const response = await fetch('https://be-todo-app-xz0m.onrender.com/api/tasks/all');
+    if (response.status !== 200) {
+      throw new Error('can not fetch the data now');
+    }
+    const data = await response.json();
+    setTodos(data.tasks)
   };
 
-const fetchAllData = async () => {
-  const response = await fetch('https://be-todo-app-xz0m.onrender.com/api/tasks/all');
-  if(response.status !== 200) {
-    throw new Error('can not fetch that data now');
+  console.log(fetchAllData);
+
+  //post req//
+  const addTodo = async () => {
+    const response = await fetch('https://be-todo-app-xz0m.onrender.com/api/tasks/add');
+    if (response.status !== 200) {
+      throw new Error('can not fetch the data');
+    }
+    const data = await response.json();
+    setTodos(data.tasks)
+  };
+  console.log(addTodo);
+
+//Delete//
+const deleteTodo = async () => {
+  const response = await fetch('https://be-todo-app-xz0m.onrender.com/api/tasks/:id');
+  if (response.status !== 200) {
+    throw new Error('can the fetch data')
   }
   const data = await response.json();
   setTodos(data.tasks)
-}
-useEffect(() => {
-  fetchAllData()
-}, [])
-console.log('todos', todos)
+};
+console.log(deleteTodo);
 
 
-const AddData = async () => {
-  const response = await fetch('https://be-todo-app-xz0m.onrender.com/api/tasks');
-  if(response.status !== 200) {
-    throw new Error('can not add that data now');
-  }
-  const data = await response.json();
-  setTodos(data.tasks)
-}
+  useEffect(() => {
+    fetchAllData()
+  }, [])
 
-useEffect(() => {
-  AddData()
-}, [])
 
-// console.log('todos', todos)
+
+
+  console.log('todos', todos)
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-600">
@@ -50,18 +60,20 @@ useEffect(() => {
         <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">Todo List</h1>
 
         <div className="mb-4 flex-row gap-6 mt-10">
-        <input
+          <input
+
             type="text"
-            className="flex-2 px-4 mt-4 py-2 border border-gray-300 rounded-l-md"
+            className="flex-2 px-4 mt-4 py-2 border text-white font-bold bg-blue-950 border-gray-300 rounded-full hover:bg-blue-900"
+
             value={newTodo}
             onChange={(e) => setNewTodo(e.target.value)}
             placeholder="add a note...... !"
           />
           <button
-            // onClick={() => addTodo(todos)}
-            className="px-4 py-2 text-4xl text-white rounded-r-md hover:bg-gray-600"
+            onClick={addTodo}
+            className="px-4 py-2 text-4xl text-white rounded-full hover:bg-blue-900"
           >
-            <IoIosAddCircle className="font-extrabold h-6"/>
+            <IoIosAddCircle className="font-extrabold h-6" />
           </button>
 
 
@@ -71,7 +83,7 @@ useEffect(() => {
           {todos.map((todo) => (
             <li
               key={todo._id}
-              className={`flex justify-between items-center p-4 border rounded-md `}
+              className={`flex justify-between items-center p-4 border rounded-full `}
             >
               <span
                 className="flex-1 cursor-pointer text-white"
@@ -79,10 +91,10 @@ useEffect(() => {
                 {todo.title}
               </span>
               <button
-                onClick={() => deleteTodo(todo._id)}
-                className="ml-2 text-red-600 hover:text-red-800"
+                onClick={deleteTodo}
+                className="ml-2 text-red-600 hover:text-red-900"
               >
-                <FaDeleteLeft className="text-red-600 text-2xl"/>
+                <FaDeleteLeft className="text-red-600 hover:text-red-900 text-3xl" />
               </button>
             </li>
           ))}
